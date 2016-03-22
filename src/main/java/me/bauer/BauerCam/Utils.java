896fd3e6c9e650bcca820fff7321ebbd6cc16c83
daipenger <fobbes@web.de>
2016-03-22 11:51:27 +0100

    1.9 update: ChatComponentText renamed to TextComponentString

--- src/main/java/me/bauer/BauerCam/Utils.java
+++ src/main/java/me/bauer/BauerCam/Utils.java
@@ -5,7 +5,7 @@ import me.bauer.BauerCam.Path.Position;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.text.TextComponentString;
 
 public final class Utils {
 
@@ -66,7 +66,7 @@ public final class Utils {
 		if (player == null) {
 			return;
 		}
-		player.addChatMessage(new ChatComponentText(msg));
+		player.addChatMessage(new TextComponentString(msg));
 	}
 
 	public static int parseSafely(final String input, final int def) {

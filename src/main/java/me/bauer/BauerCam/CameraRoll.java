eee977bc11748da1c327e14ae79d8031ccceb1ae
daipenger <fobbes@web.de>
2016-08-09 16:27:34 +0200

    Adjust camera roll speed to be similar as before

--- src/main/java/me/bauer/BauerCam/CameraRoll.java
+++ src/main/java/me/bauer/BauerCam/CameraRoll.java
@@ -2,7 +2,7 @@ package me.bauer.BauerCam;
 
 public final class CameraRoll {
 
-	private static final float anglePerKeyPress = 2.5f;
+	private static final float anglePerKeyPress = 0.5f;
 
 	public static float roll = 0;
 

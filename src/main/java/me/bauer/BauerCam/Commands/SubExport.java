615982bfd69be0a32bdd040c35e76115c510f002
daipenger <fobbes@web.de>
2016-09-07 22:44:00 +0200

    Move export/import logic into their subclasses

--- src/main/java/me/bauer/BauerCam/Commands/SubExport.java
+++ src/main/java/me/bauer/BauerCam/Commands/SubExport.java
@@ -1,10 +1,51 @@
 package me.bauer.BauerCam.Commands;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import me.bauer.BauerCam.Main;
+import me.bauer.BauerCam.Utils;
+import me.bauer.BauerCam.Path.PathHandler;
+import me.bauer.BauerCam.Path.Position;
+
 public class SubExport extends ASubExportImport {
 
 	@Override
-	public void derivedExecute(final String filename) {
-		save(filename);
+	protected void derivedExecute(final String filename) {
+		final Position[] points = PathHandler.getWaypoints();
+		if (points.length == 0) {
+			Utils.sendInformation(Main.pathIsEmpty.toString());
+			return;
+		}
+
+		final File file = new File(Main.bauercamDirectory, filename + extension);
+
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, false)));
+
+			for (final Position point : points) {
+				writer.write(point.toString());
+				writer.newLine();
+			}
+
+			Utils.sendInformation(Main.exportSuccessful.toString());
+		} catch (final IOException e) {
+			Utils.sendInformation(Main.IOError.toString());
+			Utils.sendInformation(e.getMessage());
+		}
+
+		if (writer == null) {
+			return;
+		}
+		try {
+			writer.close();
+		} catch (final IOException e) {
+		}
 	}
 
 	@Override
--- src/main/java/me/bauer/BauerCam/Commands/SubImport.java
+++ src/main/java/me/bauer/BauerCam/Commands/SubImport.java
@@ -1,10 +1,58 @@
 package me.bauer.BauerCam.Commands;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import me.bauer.BauerCam.Main;
+import me.bauer.BauerCam.Utils;
+import me.bauer.BauerCam.Path.PathHandler;
+import me.bauer.BauerCam.Path.Position;
+
 public class SubImport extends ASubExportImport {
 
 	@Override
-	public void derivedExecute(final String filename) {
-		load(filename);
+	protected void derivedExecute(final String filename) {
+		final File file = new File(Main.bauercamDirectory, filename + extension);
+
+		if (!file.exists() || !file.isFile()) {
+			Utils.sendInformation(Main.fileDoesNotExist.toString());
+			return;
+		}
+
+		final ArrayList<Position> points = new ArrayList<Position>();
+		BufferedReader reader = null;
+
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+			String s;
+			while ((s = reader.readLine()) != null) {
+				final Position point = Position.fromString(s);
+				if (point == null) {
+					reader.close();
+					return;
+				}
+				points.add(point);
+			}
+
+			PathHandler.setWaypoints(points);
+			Utils.sendInformation(Main.importSuccessful.toString());
+		} catch (final IOException e) {
+			Utils.sendInformation(Main.IOError.toString());
+			Utils.sendInformation(e.getMessage());
+		}
+
+		if (reader == null) {
+			return;
+		}
+		try {
+			reader.close();
+		} catch (final IOException e) {
+		}
 	}
 
 	@Override

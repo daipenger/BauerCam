b401f80fba17abdddebc79187720ee36aeee5134
daipenger <fobbes@web.de>
2016-10-08 18:03:08 +0200

    Prepare circular path

---
+++ src/main/java/me/bauer/BauerCam/Commands/SubCircularPath.java
@@ -0,0 +1,32 @@
+package me.bauer.BauerCam.Commands;
+
+import me.bauer.BauerCam.Path.PathHandler;
+import net.minecraft.command.CommandException;
+
+public class SubCircularPath implements ISubCommand {
+
+	@Override
+	public void execute(String[] args) throws CommandException {
+		if (args.length < 3) {
+			throw new CommandException(getDescription(), new Object[0]);
+		}
+		
+		if (PathHandler.getWaypointCount() == 0) {
+			// TODO message
+			throw new CommandException("", new Object[0]);
+		}
+		
+		
+	}
+
+	@Override
+	public String getBase() {
+		return "circle";
+	}
+
+	@Override
+	public String getDescription() {
+		return "/cam circle <radius> <circles>";
+	}
+
+}
